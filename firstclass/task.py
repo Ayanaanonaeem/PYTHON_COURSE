885e429e55400614b4ae90ab83bc56587b685cad
Ayanaanonaeem <ayanaano5@gmail.com>
2025-02-25 01:25:12 +0500

    file create

---
+++ firstclass/task.py
@@ -0,0 +1,33 @@
+name="ayan,"
+print("EX:2-3")
+
+print("hello",name,"would you like to learn some python today\n")
+
+
+print("EX:2-4")
+
+print(name.upper())
+print(name.lower())
+print(name.title(),"\n")
+
+print("EX:2-5")
+
+print("Albert Einstein once said, \"A person who never made a mistake never tried anything new.\"\n")
+
+name="Albert Einstein"
+message="once said, \"A person who never made a mistake never tried anything new.\""
+
+print("EX:2-6")
+
+print(name,message,"\n")
+
+
+print("EX:2-7")
+
+
+second_name="\t Abdul Rehman\t"
+print(second_name)
+print(second_name.strip())
+print(second_name.lstrip())
+print(second_name.rstrip())
+
---
+++ main.py/main.py
@@ -0,0 +1,16 @@
+print("hello, world")
+
+name="ali"
+name="jawaid"
+age=13
+decimal=1.34
+print(type(name),type(age),type(decimal),"hello Ali hope you are good")
+
+
+print("hello \"Ali\"")
+
+print("hello \n Ali")
+
+print(name.capitalize())
+
+print(len(name))
