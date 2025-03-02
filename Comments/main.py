848cd6381e5d2d841e0b3da89ac0627d17ddb1ee
Ayanaanonaeem <ayanaano5@gmail.com>
2025-03-03 02:35:31 +0500

    file create

---
+++ Comments/main.py
@@ -0,0 +1,18 @@
+print("hello world")
+
+# this is a  single Comment 
+
+# for single line comments we use #
+
+# for multiline comments we use """
+
+""" this is a multiline comments 
+we use this for multiline comments"""
+
+
+# in triple quotation se hum print bhi karwasakte hain jitni space denge utni hi  space ayegi
+
+print(""" this is 
+      Ayan how are you doing today""")
+
+
