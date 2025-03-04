3e4409ce0e050ffb843e3b0d7dc09704ee8d988e
Ayanaanonaeem <ayanaano5@gmail.com>
2025-03-04 23:51:37 +0500

    file create

---
+++ LECTURE_3/Ternary_Operator/main.py
@@ -0,0 +1,17 @@
+# ternary Operator means if else in one line
+
+x=int(input("enter a number"))
+
+print("Even"if x%2==0 else"Odd")
+
+y=int(input("enter a number"))
+
+print("odd"if y%2==1 else"even")
+
+
+# hum pehle print statement likhte hain matlab condition ke true hone ke bad jo print karwana hai phr if aur uske bd condition 
+# and then else aur condition ke false hone pr jo print karwana hai
+
+z=int(input("enter a number"))
+
+print("even"if z%2==0 else "odd")
