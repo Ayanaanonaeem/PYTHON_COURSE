8331da1381b4d6a0f88808d4c8df73af97e38f78
Ayanaanonaeem <ayanaano5@gmail.com>
2025-03-08 00:03:21 +0500

    file create

--- LECTURE_4/While_loop_Python/main.py
+++ LECTURE_4/While_loop_Python/main.py
@@ -1,14 +1,22 @@
 
-i=0
-while(i<4):
-    print(i)
-    i=i+1
+# i=0
+# while(i<4):
+#     print(i)
+#     i=i+1
 
-i=int(input("Enter a Number"))
-while(i<5):
-    i=int(input("Enter a Number"))
-    print(i)
+# i=int(input("Enter a Number"))
+# while(i<5):
+#     i=int(input("Enter a Number"))
+#     print(i)
 
 
-print("we are outside the loop")
+# print("we are outside the loop")
+
+count=0
+
+while(count>0):
+    print(count)
+    count=count-1
     
+else:
+    print("we are outside the loop")
