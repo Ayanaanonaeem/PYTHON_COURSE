3572c03868b53e4489a13ba87e3c25da19cbd4f9
Ayanaanonaeem <ayanaano5@gmail.com>
2025-03-07 23:57:46 +0500

    file create

--- LECTURE_4/While_loop_Python/main.py
+++ LECTURE_4/While_loop_Python/main.py
@@ -3,3 +3,12 @@ i=0
 while(i<4):
     print(i)
     i=i+1
+
+i=int(input("Enter a Number"))
+while(i<5):
+    i=int(input("Enter a Number"))
+    print(i)
+
+
+print("we are outside the loop")
+    
