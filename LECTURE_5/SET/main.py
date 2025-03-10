81fec116a5230f93e5e92e872c1a773e67f56c2e
Ayanaanonaeem <ayanaano5@gmail.com>
2025-03-11 02:35:55 +0500

    file create

---
+++ LECTURE_5/SET/main.py
@@ -0,0 +1,18 @@
+# set allow nhi krta ke koi duplicate iske undar ho is wjh se isko jb print kra to 8 nhi aya ismain
+numbers={1,2,3,4,5,6,7,8,8}
+print(numbers)
+
+mic={1,0,True,"ali",0.13}
+
+
+# for loop in set
+for i in mic:
+    print(i)
+
+print(mic)
+
+# difference is liye use hota hai take bata sake in donon main kia same hai
+set1={1,2,3,4}
+set2={2,3}
+print(set.difference(set2))
+
