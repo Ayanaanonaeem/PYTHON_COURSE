eae2b661ba6bd2e7dbcf6faa24110eb72c9fb0b7
Ayanaanonaeem <ayanaano5@gmail.com>
2025-03-12 03:48:41 +0500

    file create

--- LECTURE_6/FUNCTION/main.py
+++ LECTURE_6/FUNCTION/main.py
@@ -14,6 +14,29 @@ greet_name("Umer")
 greet_name("Taha")
 
 
+def sum_Function(a,b):
+    print(a+b)
+
+sum_Function(9,2)
+
+def mul_Function(a,b):
+    print(a*b)
+
+mul_Function(9,2)
+
+def sub_Function(a,b):
+    print(a-b)
+
+sub_Function(10,11)
+
+def div_Function(a,b):
+    print(a/b)
+
+div_Function(10,11)
+
+
+
+
 
 
 
