25b4c5731c7331d777bb9363448f2b0212ea98fd
Ayanaanonaeem <ayanaano5@gmail.com>
2025-03-13 22:19:45 +0500

    file create

---
+++ LECTURE_7/main.py
@@ -0,0 +1,7 @@
+# hum call krte huwe ek se zyada values bhi dalsakte hai lkn uskeliye function bnate waqt parameter main star lagana zarrori hai
+
+
+def func(*n1):
+    print(n1)
+
+func(1,2,3,4,5,6)
