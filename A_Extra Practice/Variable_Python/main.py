47f043705fdbeea787e31e0dbfc513fb42ac6bc9
Ayanaanonaeem <ayanaano5@gmail.com>
2025-03-19 22:56:44 +0500

    file create

---
+++ A_Extra Practice/Variable_Python/main.py
@@ -0,0 +1,13 @@
+# Variable 
+# ye hamare pass just like ek container hota hai jis main apni values store karsakte hain
+
+name="Ayan"
+a=67
+b=True
+
+print(a)
+print(name)
+print(b)
+
+# name ek variable hai jis main Ayan store huwa wa hai
+# a bhi ek variable hai jis main 67 store huwa wa hai
