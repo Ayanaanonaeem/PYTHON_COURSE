dde8f4193136ee25a05834b3f391030c21c77899
Ayanaanonaeem <ayanaano5@gmail.com>
2025-03-19 22:44:09 +0500

    file create

---
+++ A_Extra Practice/Comment_Python/main.py
@@ -0,0 +1,12 @@
+# comment hamare pass ye hota hai jis block of code ko ap execute nhi krna chahte to ap use comment krden 
+
+# for single line comment we use #
+
+# for multiline comments we use this ''' or we use this """
+
+'''ye ek comment hai hum comment ke bare main 
+bare main parh rahe hain '''
+
+""" ye ek comment hai hum comment ke bare main 
+bare main parh rahe hain """
+
