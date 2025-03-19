578ad8f1f0075be3690285a68fb363c22af68d4c
Ayanaanonaeem <ayanaano5@gmail.com>
2025-03-19 22:39:30 +0500

    file create

--- A_Extra Practice/Print_Statement/main.py
+++ A_Extra Practice/Print_Statement/main.py
@@ -1,2 +1,12 @@
 print("hello world")
 print("Ayan")
+
+print("hello world")
+
+
+# print ke undar aur bhi parameter hote hain jismain se ek sep hota hai aur ek end 
+# 1. sep hamare pass seperator hota hai hum sep likh kr jisse seperate krna chahen krsakte hain
+# 2. aur end hamare pass ye kam krte hai ke hamara print statement ka ending point kia hoga bydefault is
+#  main backslack n hota hai jo ke hamare pass ek escape sequence character hai
+print(7,4,5,6,sep=":",end="end")
+print("hello")
