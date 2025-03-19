ce4a58ca4a28ef607a3c00888a814a8736a0e06f
Ayanaanonaeem <ayanaano5@gmail.com>
2025-03-20 04:30:36 +0500

    file create

---
+++ A_Extra Practice/Excercises/Excercise no 1/main.py
@@ -0,0 +1,17 @@
+a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+sign=input("enter operation")
+
+if sign=="+":
+    print(a+b)
+elif sign=="-":
+    print(a-b)
+elif sign=="*" or sign=="x":
+    print(a*b)
+elif sign=="/":
+    print(a/b)
+else:
+    print("reload the page and enter number again")
+
+
+
